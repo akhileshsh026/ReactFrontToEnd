e370cc3aedd6fcefa8754e1bb0fff14a549a404b
akhileshsh026 <akhileshsh026@hotmail.com>
2019-06-20 23:08:29 +0530

    Filter command

--- main.js
+++ main.js
@@ -8,3 +8,18 @@ const result = fruits.map(element => {
   element.slice(0, -1);
 });
 console.log(result);
+
+// Filter
+
+const people = [
+  { id: 1, name: "Akhilesh" },
+  { id: 2, name: "Kumar" },
+  { id: 3, name: "Sahu" },
+  { id: 4, name: "Akhi" }
+];
+
+const filterResult = people.filter(element => {
+  if (element.id != 2) {
+    console.log(element.name);
+  }
+});
