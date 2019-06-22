8b1b9707d4fe9b279b38e993678165ecabb1c10b
akhileshsh026 <akhileshsh026@hotmail.com>
2019-06-22 21:35:26 +0530

    porps in functional components littile different

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 function Header(props) {
+  const { branding } = props;
   return (
     <div>
-      <h1>{props.branding}</h1>
+      <h1>{branding}</h1>
     </div>
   );
 }
