c4cb59805177f06cec6e1411905e2bd7fe58613f
akhileshsh026 <akhileshsh026@hotmail.com>
2019-06-22 22:02:17 +0530

    another way to validate porps but 1 st one seprate one is good option

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired
+  };
+
   render() {
     const { name, email, phone } = this.props;
     return (
@@ -16,10 +22,10 @@ class Contact extends Component {
   }
 }
 
-Contact.propTypes = {
-  name: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  phone: PropTypes.string.isRequired
-};
+// Contact.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   email: PropTypes.string.isRequired,
+//   phone: PropTypes.string.isRequired
+// };
 
 export default Contact;
