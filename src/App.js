1f8ab9155d291f6f992d1c2a3d2644e805710212
akhileshsh026 <akhileshsh026@hotmail.com>
2019-06-22 17:52:59 +0530

    JSX are in action. how the html code is transformed in js.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1> This is All cleaned Project </h1>
-    </div>
+  return React.createElement(
+    "div",
+    { className: "App" },
+    React.createElement("h1", null, "This is from the JSX Syntax.")
   );
+
+  // return (
+  //   <div className="App">
+  //     <h1> This is All cleaned Project </h1>
+  //   </div>
+  // );
 }
 
 export default App;
