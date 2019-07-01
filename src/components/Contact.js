314d961877af9f46608a7ffa65252c16e0fa4944
akhileshsh026 <akhileshsh026@hotmail.com>
2019-07-01 09:50:33 +0530

    This is how state chnages within Components.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,24 +7,28 @@ class Contact extends Component {
     showContactInfo: true
   };
 
-  OnShowClick = e => {
-    this.setState({
-      showContactInfo: !this.state.showContactInfo
-    });
-  };
-
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
         <h4>
           {name}
-          <MaterialIcon onClick={this.OnShowClick} icon="arrow_drop_down" />
+          <MaterialIcon
+            onClick={() => {
+              this.setState({
+                showContactInfo: !this.state.showContactInfo
+              });
+            }}
+            icon="arrow_drop_down"
+          />
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email : {email}</li>
-          <li className="list-group-item">Phone : {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email : {email}</li>
+            <li className="list-group-item">Phone : {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
